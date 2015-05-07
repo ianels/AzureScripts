def08f2911e817af3cff6a9cd455180b019891e6
ianels <ianels@outlook.com>
2015-05-07 10:48:36 -0400

    UpdateVM
    Script uploaded to update a VM

---
+++ UpdateVMExample.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module Azure
+
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+Get-AzureVM -ServiceName "MySvc1" -Name "MyVM3" | 
+Set-AzureVMExtension -Publisher CloudLink.SecureVM -ExtensionName CloudLinkSecureVMLinuxAgent -Version 3.* -PublicConfigPath "$scriptDir\CloudLink.config" | 
+Update-AzureVM
