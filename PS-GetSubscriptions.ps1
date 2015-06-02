6f65c407b8d3e275129ad83d3fb5ab1e9353d12d
ianels <ianels@outlook.com>
2015-06-02 10:59:08 -0400

    Subscription Selection

---
+++ PS-GetSubscriptions.ps1
@@ -0,0 +1,28 @@
+﻿Function PS-GetSubscriptions {
+
+Try {
+
+    Import-Module Azure
+
+    $subscriptionList = Get-AzureSubscription
+
+    ForEach ($subscription in $subscriptionList) {
+        $subscriptionNumber ++
+
+        $properties = @{'SubscriptionNumber'=$subscriptionNumber;
+            'SubscriptionName'=$subscription.SubscriptionName
+            'SubscriptionID'=$subscription.SubscriptionId
+            }
+        $PSObject = New-Object -TypeName PSObject -Property $properties
+        $subscriptionResults += @($PSObject)
+        }
+
+    return $subscriptionResults
+
+    }
+
+    Catch [system.exception] {
+    Write-Output 'Unable to retrieve Azure Subscriptions. Check your settings and try again or contact your subscription administrator.'
+    }
+
+}
---
+++ PS-SetActiveSubscription.ps1
@@ -0,0 +1,29 @@
+﻿Function PS-SetActiveSubscription {
+Param(
+[object] $SubscriptionToSetActive
+)
+
+Import-Module Azure
+
+Try{
+
+$subscriptionIDToSetActive = $subscriptionToSetActive.SubscriptionID
+$subscriptionNameToSetActive = $subscriptionToSetActive.subscriptionName
+
+Write-Host "Setting $subscriptionNameToSetActive as the active subscription..."
+Select-AzureSubscription -SubscriptionId $subscriptionIDToSetActive
+Write-Host "$subscriptionNameToSetActive is set as the active subscription."
+}
+
+Catch{
+    Write-Host 'Error occured while attempting to create Storage Account'
+    #Return the error message
+    Return $_.
+}
+
+Finally{
+    Clear-Variable -Name subscriptionIDToSetActive -ErrorAction SilentlyContinue
+    Clear-Variable -Name subscriptionNameToSetActive -ErrorAction SilentlyContinue
+}
+
+}
