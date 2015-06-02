fb64cdad4fdc73aee5e18d214c03be43cd9e4467
ianels <ianels@outlook.com>
2015-06-02 09:33:22 -0400

    Connect to Azure
    Uploading Connect to Azure sub function

---
+++ PS-ConnectToAzure.ps1
@@ -0,0 +1,26 @@
+﻿Function PS-ConnectToAzure {
+Param(
+[Parameter(Mandatory=$true)][string] $AzureUserName,
+[Parameter(Mandatory=$true)][System.Security.SecureString]$AzurePassword
+)
+Import-Module Azure
+
+$azureCred = New-Object System.Management.Automation.PSCredential($AzureUserName,$AzurePassword)
+
+Try {
+Write-Host '\\\\\\\\\\\'
+Write-Host 'Connecting to Azure...'
+
+Add-AzureAccount -Credential $azureCred
+ 
+Write-Host 'Connection to Azure established'
+Write-Host '///////////'
+}
+
+Catch [system.exception] {
+Write-Host 'Unable to authenticate to Azure. Check your settings and try again or contact your subscription administrator.'
+Write-Host '///////////'
+}
+
+return $azureCred | Out-Null
+}
