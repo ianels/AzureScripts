042a626525b13df0d3bf5a51f12f48926b99a338
ianels <ianels@outlook.com>
2015-06-03 11:41:19 -0400

    Subscription mgmt subroutines

---
+++ SubscriptionMgmtandAuth/PS-ClearCachedSubscriptions.ps1
@@ -0,0 +1,16 @@
+﻿Function PS-ClearCachedSubscriptions {
+Param(
+[PSObject] $SubscriptionList
+)
+Import-Module Azure
+
+Try {
+    ForEach ($subscription in $SubscriptionList) {
+        Remove-AzureSubscription -SubscriptionName $subscription.SubscriptionName -Force -WarningAction SilentlyContinue | Out-Null
+        }
+    }
+Catch
+    {
+    Return $_
+    }
+}
